55db3d631b68f1af48f357ca91d9822ae163ed55
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-03-02 01:10:14 +0300

    Clean up a test

--- src/test/java/com/ilysenko/nloops/NLoopsTest.java
+++ src/test/java/com/ilysenko/nloops/NLoopsTest.java
@@ -1,7 +1,5 @@
 package com.ilysenko.nloops;
 
-import java.util.Arrays;
-
 public class NLoopsTest {
 
     public static void main(String[] args) {
@@ -11,13 +9,5 @@ public class NLoopsTest {
                 .from(2).to(4)
                 .action(System.out::println);
         System.out.println();
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 1; j < 3; j++) {
-                for (int k = 2; k < 4; k++) {
-                    System.out.println(Arrays.asList(i, j, k));
-                }
-            }
-        }
     }
 }
