e222dc9b9919eec8b6ee2329c0df098be67e0a6f
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-03-02 23:50:21 +0300

    Add unit tests

--- src/test/java/com/ilysenko/nloops/NLoopsTest.java
+++ src/test/java/com/ilysenko/nloops/NLoopsTest.java
@@ -1,25 +1,51 @@
 package com.ilysenko.nloops;
 
-public class NLoopsTest {
+import org.junit.jupiter.api.Test;
 
-    public static void main(String[] args) {
-        test1();
-        System.out.println();
-        test2();
-    }
+import java.util.Arrays;
 
-    private static void test1() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NLoopsTest {
+
+    @Test
+    void testLoop1() {
+        StringBuilder target = new StringBuilder();
+        StringBuilder source = new StringBuilder();
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 1; j < 3; j++) {
+                for (int k = 2; k < 4; k++) {
+                    target.append(Arrays.asList(i, j, k));
+                }
+            }
+        }
+
         new NLoops()
                 .from(0).to(2)
                 .from(1).to(3)
                 .from(2).to(4)
-                .action(System.out::println);
+                .action(source::append);
+
+        assertEquals(target.toString(), source.toString());
     }
 
-    private static void test2() {
+    @Test
+    void testLoop2() {
+        StringBuilder target = new StringBuilder();
+        StringBuilder source = new StringBuilder();
+
+        for (int i = 0; i < 3; i += 2) {
+            for (int j = 5; j > 2; j--) {
+                target.append(Arrays.asList(i, j));
+            }
+        }
+
         new NLoops()
                 .from(0).to(3).inc(2)
                 .from(5).to(2).dec(1)
-                .action(System.out::println);
+                .action(source::append);
+
+        assertEquals(target.toString(), source.toString());
     }
 }
