e9e2892289f212b59872c2c15823abfd055dcf63
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-03-04 00:19:26 +0300

    Add tests

--- src/test/java/com/ilysenko/nloops/LoopsTest.java
+++ src/test/java/com/ilysenko/nloops/LoopsTest.java
@@ -3,6 +3,7 @@ package com.ilysenko.nloops;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,7 +32,7 @@ class LoopsTest {
     }
 
     @Test
-    void testLoosp2() {
+    void testLoops2() {
         StringBuilder target = new StringBuilder();
         StringBuilder source = new StringBuilder();
 
@@ -50,6 +51,60 @@ class LoopsTest {
                 .from(7).to(2)
                 .from(18).to(5).step(3)
                 .from(1).to(4)
+                .action(source::append);
+
+        assertEquals(target.toString(), source.toString());
+    }
+
+    @Test
+    void testLoops3() {
+        StringBuilder target = new StringBuilder();
+        StringBuilder source = new StringBuilder();
+
+        for (int i = 0; i < 3; i += 2) {
+            for (int j = 7; j > 2; j--) {
+                for (int k = 18; k > 5; k -= 3) {
+                    for (int l = 1; l < 4; l++) {
+                        target.append(Arrays.asList(i, j, k, l));
+                    }
+                }
+            }
+        }
+
+        List<Loop> loops = Arrays.asList(
+                new Loop(0, 3, 2),
+                new Loop(7, 2),
+                new Loop(18, 5, 3),
+                new Loop(1, 4));
+
+        new Loops(loops)
+                .action(source::append);
+
+        assertEquals(target.toString(), source.toString());
+    }
+
+    @Test
+    void testLoops4() {
+        StringBuilder target = new StringBuilder();
+        StringBuilder source = new StringBuilder();
+
+        for (int i = 0; i < 3; i += 2) {
+            for (int j = 7; j > 2; j--) {
+                for (int k = 18; k > 5; k -= 3) {
+                    for (int l = 1; l < 4; l++) {
+                        target.append(Arrays.asList(i, j, k, l));
+                    }
+                }
+            }
+        }
+
+        List<Loop> loops = Arrays.asList(
+                new Loop().from(0).to(3).step(2),
+                new Loop().from(7).to(2),
+                new Loop().from(18).to(5).step(3),
+                new Loop().from(1).to(4));
+
+        new Loops(loops)
                 .action(source::append);
 
         assertEquals(target.toString(), source.toString());
