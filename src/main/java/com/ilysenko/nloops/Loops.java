9635c27404d9eeeedfd64f2f69c792370a8c30b4
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-03-09 03:24:44 +0300

    Add copyright

--- src/main/java/com/ilysenko/nloops/Loops.java
+++ src/main/java/com/ilysenko/nloops/Loops.java
@@ -1,3 +1,11 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Ilya Lysenko
+ *
+ * A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ */
 package com.ilysenko.nloops;
 
 import java.util.ArrayList;
